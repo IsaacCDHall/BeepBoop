18aae1d154e82090bfd14168b043e1afbd3fec51
Isaac Hall <isaacdehall@gmail.com>
2019-01-25 09:13:14 -0800

    add tentative functionality by means of storing submission input

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+
+// user interface logic
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var formInput = parseInt($("input").val());
+    console.log(formInput);
+    for (var index = 0; index <= formInput; index += 1) {
+      console.log(index);
+    }
+
+
+  });
+});
