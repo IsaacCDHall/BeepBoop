0cbefc09749b3ddc588d4df3a602fa8500929d61
Isaac Hall <isaacdehall@gmail.com>
2019-01-25 12:22:30 -0800

    entirely revamp javascript to utilize replace method rather than index

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 var theCount = [];
-
-// var x = "0";
-// var z = [];
-// function find(haystack, needle) {
-//     return !!~haystack.indexOf(needle);
-// };
-
-
-
+function replaceOnes(str){
+	return noOnes = str.replace(/1/g, "Beep!");
+}
+function replaceTwos(str){
+	return noTwos = str.replace(/2/g, "Boop!");
+}
+function replaceThrees(str){
+	return noThrees = str.replace(/3/g, "I'm sorry Isaac. I can't do that");
+}
 
 // user interface logic
 $(document).ready(function(){
@@ -15,25 +15,13 @@ $(document).ready(function(){
     event.preventDefault();
     var formInput = parseInt($("input").val());
     for (var index = 0; index <= formInput; index += 1) {
-      var findOne = theCount.indexOf(1);
-      var findTwo = theCount.indexOf(2);
-      var findThree = theCount.indexOf(3);
-      if (findThree !== -1) {
-        theCount[findThree] = " I'm Sorry, Isaac. I'm afraid I can't do that";
-      } else if (findTwo !== -1) {
-        theCount[findTwo] = " Boop!";
-      } else if (findOne !== -1) {
-        theCount[findOne] = " Beep!";
-      }
+      console.log("hello" + index);
       theCount.push(index);
     }
-    $(".boop").text(theCount);
+    console.log(theCount);
+    replaceOnes(theCount.toString());
+    replaceTwos(noOnes);
+    replaceThrees(noTwos);
+    $(".boop").text(noThrees);
   });
 });
-
-
-
-// Ideas for changing numbers:
-//   Change theCount toString and then .replace(1|2|3)
-//   indexOf check, replace
-//   needle in haystack find
