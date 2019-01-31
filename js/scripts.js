2914289b906d3b54337093e87cf5f3ba91e2596d
Isaac Hall <isaacdehall@gmail.com>
2019-01-30 22:52:14 -0800

    separate business logic from UI and remove global variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-var theCount = [];
-var nameInput;
+
 function findThrees(num){
   return num.toString().indexOf('3') > -1 //return true or false
 }
@@ -9,31 +8,33 @@ function findTwos(num){
 function findOnes(num){
   return num.toString().indexOf('1') > -1 //return true or false
 }
+var priorities = function(numInput, theCount, nameInput){
+  for (var index = 0; index <= numInput; index += 1) {
+    if (findThrees(index)){
+      theCount.push(" I'm sorry, " + nameInput + ". I'm afraid I can't do that.");
+    } else if (findTwos(index)){
+      theCount.push(" Boop!" );
+    }else if (findOnes(index)){
+      theCount.push(" Beep!");
+    }else {
+      theCount.push(index);
+    }
+  };
+};
 // user interface logic
 $(document).ready(function(){
   $('button').on('click',function(){
     $('.boop').addClass('animated slideInUp');
     $('div.top').addClass('animated fadeOut');
   });
-
   $("form").submit(function(event){
     event.preventDefault();
     var numInput = parseInt($("input").val());
     var nameInput = $("#name").val();
+    var theCount = [];
     $("#easteregg").hide();
-    for (var index = 0; index <= numInput; index += 1) {
-      if (findThrees(index)){
-        theCount.push(" I'm sorry, " + nameInput + ". I'm afraid I can't do that.");
-      } else if (findTwos(index)){
-        theCount.push(" Boop!" );
-      }else if (findOnes(index)){
-        theCount.push(" Beep!");
-      }else {
-        theCount.push(index);
-      }
-    };
+    priorities(numInput, theCount, nameInput);
     if (nameInput.toLowerCase() === "boop"){
-      nameInput = "bOoP!!"
       $('body').addClass("booped");
       $("#easteregg").show();
       theCount.reverse();
